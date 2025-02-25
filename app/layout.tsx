3c6af85f95ad6f8961385487fcd544e3318edec6
Fastech-It <info@fastech.services>
2025-02-25 19:27:39 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="light">
       <head>
         <link rel="icon" className='rounded-full' href={logo.src} sizes="32x32" />
       </head>
